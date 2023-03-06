6dbcb798c93ef435847ab826d6299468a835d789
holomdev <holom.dev@gmail.com>
2023-03-06 17:21:40 -0600

    add standarjs

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,15 +1,15 @@
-import { ShopLayout } from "@/components/layouts"
-import { Box, Typography } from "@mui/material"
+import { ShopLayout } from '@/components/layouts'
+import { Box, Typography } from '@mui/material'
 
 const Custom404 = () => {
   return (
-    <ShopLayout title={"Page not found"} pageDescription={"No hay nada que mostrar aqui"}>
-      <Box 
-        display="flex" 
-        justifyContent="center" 
-        alignItems="center" 
+    <ShopLayout title={'Page not found'} pageDescription={'No hay nada que mostrar aqui'}>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
         height="calc(100vh - 200px)"
-        sx={{ flexDirection: { xs: "column", sm: "row" }}}
+        sx={{ flexDirection: { xs: 'column', sm: 'row' } }}
       >
         <Typography variant="h1" component="h1" fontSize={80} fontWeight={200}>404 | </Typography>
         <Typography marginLeft={2}>No entramos el contenido solicitado</Typography>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,18 @@
 
-import { NextPage } from "next"
-import { Card, CardActionArea, CardMedia, Grid, Typography } from "@mui/material"
-import { ShopLayout } from "@/components/layouts"
-import { initialData } from '../database/products';
+import { NextPage } from 'next'
+import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material'
+import { ShopLayout } from '@/components/layouts'
+import { initialData } from '../database/products'
 
 const Home: NextPage = () => {
   return (
     <ShopLayout title={'Teslo-Shop - Home'} pageDescription={'Encuentra los mejores productos'}>
       <Typography variant="h1" component="h1">Tienda</Typography>
-      <Typography variant="h2" sx={{ mb: 1}}>Todos los productos</Typography>
+      <Typography variant="h2" sx={{ mb: 1 }}>Todos los productos</Typography>
 
       <Grid container spacing={4}>
         {
-          initialData.products.map( product => (
+          initialData.products.map(product => (
             <Grid item xs={6} sm={4} key={product.slug}>
               <Card>
                 <CardActionArea>
@@ -31,4 +31,4 @@ const Home: NextPage = () => {
     </ShopLayout>
   )
 }
- export default Home
+export default Home
